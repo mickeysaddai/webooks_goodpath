402226b54970b04c27a9f9a4af3df59151f70800
Michelle Addai <88467155+mickeysaddai@users.noreply.github.com>
2022-03-30 14:21:42 -0400

    put endpoint working

--- routes/posts.js
+++ routes/posts.js
@@ -7,8 +7,8 @@ router.get('/', postsController.getAllPostsController)
 router.get('/user/:user_id', postsController.getUserPostsController)
 router.get('/:id', postsController.getSinglePostController)
 router.post('/',postsController.authenticatePostController)
-router.delete("/:id", postsController.deleteSinglePostController)
-router.put("/:id", postsController.putSinglePostController)
+router.delete('/:id', postsController.deleteSinglePostController)
+router.put('/:id', postsController.putSinglePostController)
 
 
 
